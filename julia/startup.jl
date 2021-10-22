b05ee7f51f301128e94bb7c0100cf89db14d52b3
icweaver <iweaver@cfa.harvard.edu>
2021-10-21 21:25:00 -0400

    up

--- julia/startup.jl
+++ julia/startup.jl
@@ -1,11 +1,9 @@
 import REPL
 import Pkg
 
-if isfile("Project.toml")
-    Pkg.activate(".")
-    Base.atreplinit() do repl
-        repl.interface = REPL.setup_interface(repl)
-        repl.interface.modes[1].prompt =
-            () -> string(Pkg.REPLMode.promptf()[1:end-5], "julia> ")
-    end
+Pkg.activate(Base.current_project())
+Base.atreplinit() do repl
+    repl.interface = REPL.setup_interface(repl)
+    repl.interface.modes[1].prompt =
+        () -> string(Pkg.REPLMode.promptf()[1:end-5], "julia> ")
 end
