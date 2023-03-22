de96da2c908893101e2100bfbc9f9024302079ae
icweaver <weaveric@gmail.com>
2023-03-22 13:42:22 -0700

    up

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 require "paq" {
     "savq/paq-nvim",
     "navarasu/onedark.nvim",
-    --{"nvim-treesitter/nvim-treesitter", run=":TSUpdate"},
+    {"nvim-treesitter/nvim-treesitter", run=":TSUpdate"},
     "JuliaEditorSupport/julia-vim",
     "ellisonleao/glow.nvim",
 }
@@ -60,37 +60,32 @@ end
 
 nmap("<esc>", ":noh<cr><esc>")
 
----- Treesitter
---require'nvim-treesitter.configs'.setup {
---  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
---  ignore_install = { }, -- List of parsers to ignore installing
---  highlight = {
---    enable = { "julia" },              -- false will disable the whole extension
---    disable = { },  -- list of language that will be disabled
---    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
---    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
---    -- Using this option may slow down your editor, and you may see some duplicate highlights.
---    -- Instead of true it can also be a list of languages
---    additional_vim_regex_highlighting = false,
---  },
---  incremental_selection = {
---    enable = true,
---    keymaps = {
---      init_selection = "gnn",
---      node_incremental = "grn",
---      scope_incremental = "grc",
---      node_decremental = "grm",
---    },
---  },
---  indent = {
---    enable = true
---  }
---}
-
--- LaTex
--- g["vimtex_compiler_latexmk"] = {build_dir="./build"}
--- g["vimtex_view_general_viewer"] = "okular"
--- g["vimtex_view_general_options"] = "--unique file:@pdf\\#src:@line@tex"
+-- Treesitter
+require'nvim-treesitter.configs'.setup {
+  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+  ignore_install = { }, -- List of parsers to ignore installing
+  highlight = {
+    enable = { "julia" },              -- false will disable the whole extension
+    disable = { },  -- list of language that will be disabled
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+  indent = {
+    enable = true
+  }
+}
 
 -- Disabled providers}
 g["loaded_python3_provider"] = 0
