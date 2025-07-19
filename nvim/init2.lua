a4b09600944a125f1ec686b0f698f1be80739ec7
icweaver <weaveric@gmail.com>
2025-07-19 13:40:58 -0700

    up

---
+++ nvim/init2.lua
@@ -0,0 +1,45 @@
+require "paq" {
+    "savq/paq-nvim",
+    {"nvim-treesitter/nvim-treesitter", run=":TSUpdate"},
+    "JuliaEditorSupport/julia-vim",
+    "ellisonleao/gruvbox.nvim",
+    "ellisonleao/glow.nvim",
+    "f-person/auto-dark-mode.nvim"
+}
+
+-- Aliases
+local opt = vim.opt
+local g = vim.g
+local cmd = vim.cmd
+local api = vim.api
+
+require("gruvbox").setup({
+  contrast = "hard",
+  dim_inactive = true,
+  transparent_mode = true,
+})
+
+require("auto-dark-mode").setup({
+    set_dark_mode = function()
+        opt.background = "dark"
+    end,
+    set_light_mode = function()
+        opt.background = "light"
+    end,
+    update_interval = 2000,
+    fallback = "dark"
+})
+
+opt.list = true
+opt.expandtab = true
+opt.autoindent = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.title = true
+opt.titlestring = [[%F %h%r%w %m]]
+
+cmd("colo gruvbox")
+
+-- copy/paste global
+api.nvim_set_keymap("", "<leader>y", '"+y', { silent = true })
