ed087355dbdeb847397aa2a4b0f6926f1a7ee0b7
icweaver <weaveric@gmail.com>
2025-07-29 02:09:58 -0700

    up

--- julia/startup.jl
+++ julia/startup.jl
@@ -2,10 +2,8 @@ if isinteractive()
     using OhMyREPL
     darkmode = occursin("dark", read(`gsettings get org.gnome.desktop.interface gtk-theme`, String))
     if darkmode
-        @info "Setting dark theme"
         colorscheme!("GitHubDark")
     else
-        @info "Setting light theme"
         colorscheme!("GitHubLight")
     end
 end
